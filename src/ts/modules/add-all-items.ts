6ee863617bc617f53ce8e3000a847d92263c72d6
Anton Vyun <zeta664433@gmail.com>
2022-12-17 02:31:50 +0300

    faet: add fn to create good item

--- src/ts/modules/add-all-items.ts
+++ src/ts/modules/add-all-items.ts
@@ -4,8 +4,12 @@ import Product from "./create-item";
 export function createAllItems(items: TGoodsData[]): void {
   items.forEach((item) => {
     new Product(item).rendore(
-      "/",
+      `/good/${item.id}`,
       <HTMLElement>document.querySelector(".main__items-body")
     );
   });
 }
+
+export const createGood = (item: TGoodsData) => {
+  new Product(item).render();
+};
