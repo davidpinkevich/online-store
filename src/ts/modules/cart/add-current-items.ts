0643b52c6e1ee5aaacdd04c8086d9b4ef4af57d0
davidpinkevich <davidpinkevich95@gmail.com>
2022-12-19 21:51:35 +0300

    feat: add and remove items in cart

--- src/ts/modules/cart/add-current-items.ts
+++ src/ts/modules/cart/add-current-items.ts
@@ -1,20 +1,26 @@
 import { ICartItems } from "../../types/types";
-// import { createCart } from "./cart-block";
+import { createCart } from "./cart-block";
+import { itemCost } from "./item-cost";
+import { totalPrice } from "./total-cost";
 
 function addItemCurrent(
   event: Event,
   btnAdd: HTMLButtonElement,
   btnRemove: HTMLButtonElement,
-  amount: HTMLElement
+  amount: HTMLElement,
+  allPriceForItem: HTMLElement
 ) {
   const lsCart = JSON.parse(localStorage.getItem("cart-storage") || "");
   const newLs: ICartItems[] = [];
+  const secondLs: ICartItems[] = [];
+  const mainCost = <HTMLElement>document.querySelector(".header__cost>span");
   //-----------добавление----------------------------
   if (event.target === btnAdd) {
     const curr = Number(btnAdd.parentElement?.getAttribute("id"));
     lsCart.forEach((miniItem: ICartItems) => {
       if (miniItem.id === curr && miniItem.count < miniItem.stock) {
         miniItem.count++;
+        itemCost(miniItem, allPriceForItem);
         amount.innerHTML = `Items: <span>${miniItem.count}</span>`;
         newLs.push(miniItem);
       } else {
@@ -22,24 +28,32 @@ function addItemCurrent(
       }
     });
     localStorage.setItem("cart-storage", JSON.stringify(newLs));
+    totalPrice(mainCost);
   }
   //----------удаление--------------------------------
   if (event.target === btnRemove) {
     const curr = Number(btnRemove.parentElement?.getAttribute("id"));
     lsCart.forEach((miniItem: ICartItems) => {
       if (miniItem.id === curr && miniItem.count > 0) {
+        totalPrice(mainCost);
         miniItem.count--;
+        itemCost(miniItem, allPriceForItem);
         amount.innerHTML = `Items: <span>${miniItem.count}</span>`;
         newLs.push(miniItem);
       } else {
         newLs.push(miniItem);
-        // const kek = <HTMLElement>document.getElementById(`${curr}`);
-        // const lol = <HTMLElement>kek.parentElement;
-        // lol.remove();
       }
     });
     localStorage.setItem("cart-storage", JSON.stringify(newLs));
+    totalPrice(mainCost);
   }
+  lsCart.forEach((item: ICartItems) => {
+    if (item.count > 0) {
+      secondLs.push(item);
+    }
+  });
+  localStorage.setItem("cart-storage", JSON.stringify(secondLs));
+  if (secondLs.length !== newLs.length) createCart();
   //------------------------------------------------------
 }
 
