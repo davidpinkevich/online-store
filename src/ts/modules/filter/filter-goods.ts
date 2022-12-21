b32676284f9a9289046950552e607ff31b592255
davidpinkevich <davidpinkevich95@gmail.com>
2022-12-21 20:50:25 +0300

    feat: change all items every action

--- src/ts/modules/filter/filter-goods.ts
+++ src/ts/modules/filter/filter-goods.ts
@@ -5,5 +5,8 @@ import { myCompose } from "./compose/compose";
 
 export const filterGoods = () => {
   const data: TGoodsData[] = myCompose(goodsData);
+  // очищаем блок на каждое действие и отрисовывем
+  const body = <HTMLElement>document.querySelector(".main__items-body");
+  body.innerHTML = "";
   createAllItems(data);
 };
