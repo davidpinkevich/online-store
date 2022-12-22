95f43215b9dc90354a9d518ce8057770583f6cec
davidpinkevich <davidpinkevich95@gmail.com>
2022-12-22 22:03:05 +0300

    feat: start version for change in all filters

--- src/ts/modules/filter/compose/filter-by-category.ts
+++ src/ts/modules/filter/compose/filter-by-category.ts
@@ -5,6 +5,7 @@ export const filterByCategory: TCompose = (
   data: TGoodsData[]
 ): TGoodsData[] => {
   if (filterStore.categories.length === 0) return data;
+
   return filterStore.categories
     .map((category) => {
       return data.filter((datum) => {
@@ -12,4 +13,27 @@ export const filterByCategory: TCompose = (
       });
     })
     .flat();
+
+  //   const brandsCheckbox: NodeListOf<HTMLInputElement> =
+  //   document.querySelectorAll(".filter__brand .checkbox__input");
+  // const arr: TGoodsData[] = [];
+  // filterStore.categories.forEach((miniItem: string) => {
+  //   data.forEach((item: TGoodsData) => {
+  //     if (item.category === miniItem) arr.push(item);
+  //   });
+  // });
+
+  // arr.forEach((itemSort: TGoodsData) => {
+  //   brandsCheckbox.forEach((item) => {
+  //     if (itemSort.brand !== item.getAttribute("id")) {
+  //       item.classList.toggle("diss");
+  //       item.disabled = true;
+  //     }
+  //   });
+  // });
+  // if (arr.length === 0) {
+  //   return data;
+  // } else {
+  //   return arr;
+  // }
 };
