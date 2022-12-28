afd992e28efb9154f30b74156c396c704a11b9a6
Anton Vyun <zeta664433@gmail.com>
2022-12-28 22:36:46 +0300

    feat: push query string

--- src/ts/modules/change-view/ls-change-for-view.ts
+++ src/ts/modules/change-view/ls-change-for-view.ts
@@ -1,16 +1,23 @@
+import { addSearchQueryString } from "../filter/query/create-query-string";
+import { readQueryString } from "../filter/query/read-query-string";
 import { changeView } from "./change-view";
 
 export function changeViewItems(): void {
   const one = <HTMLButtonElement>document.querySelector(".search__change-one");
   const two = <HTMLButtonElement>document.querySelector(".search__change-two");
 
-  // const lsStore = localStorage.getItem("view-items");
   one.addEventListener("click", () => {
-    localStorage.setItem("view-items", "one");
+    two.classList.remove("search__change-two_active");
+    one.classList.add("search__change-one_active");
+    addSearchQueryString("big", "true");
+    readQueryString();
     changeView();
   });
   two.addEventListener("click", () => {
-    localStorage.setItem("view-items", "two");
+    one.classList.remove("search__change-one_active");
+    two.classList.add("search__change-two_active");
+    addSearchQueryString("big", "false");
+    readQueryString();
     changeView();
   });
 }
