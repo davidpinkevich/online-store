eef0c1b80e11c75fbe2bae726defbfd2d557294a
Anton Vyun <zeta664433@gmail.com>
2023-01-04 13:53:25 +0300

    feat: call fn validate

--- src/ts/modules/modal-window/add-window.ts
+++ src/ts/modules/modal-window/add-window.ts
@@ -1,10 +1,15 @@
+import { checkCvv } from "./checks/check-cvv";
+import { checkDate } from "./checks/check-date";
+import { checkName } from "./checks/check-name";
+import { checkNumber } from "./checks/check-number";
+
 export function createModalWindow(): void {
   const mod = document.createElement("div");
   mod.classList.add("modal");
   mod.innerHTML = `<h2 class="modal__title">Personal data</h2>
       <div class="modal__body">
         <input type="text" class="modal__body-name" placeholder="Full name">
-        <input type="number" class="modal__body-phone" placeholder="Phone number">
+        <input type="tel" class="modal__body-phone" placeholder="Phone number">
         <input type="text" class="modal__body-address" placeholder="Delivery address">
         <input type="email" class="modal__body-email" placeholder="E-mail">
       </div>
@@ -12,10 +17,15 @@ export function createModalWindow(): void {
       <div class="modal__card">
         <input type="number" class="modal__card-number" placeholder="Card number">
         <div class="modal__card-footer">
-          <input type="number" class="modal__card-footer-valid" placeholder="Date">
+          <input type="number" min="1" max="12" class="modal__card-footer-month" placeholder="Month">
+          <input type="number" class="modal__card-footer-year" placeholder="Year">
           <input type="number" class="modal__card-footer-cvv" placeholder="cvv">
         </div>
       </div>
       <button class="modal__btn">CONFIRM</button>`;
   document.body.after(mod);
+  checkName();
+  checkNumber();
+  checkDate();
+  checkCvv();
 }
